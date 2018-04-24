531bc4b97686973428fa2dc94a977356bb8ab0ef
Xueying Zheng <zhengxueyingbupt@gmail.com>
2018-04-24 19:09:39 -0400

    remove duplicate code

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,37 +1,3 @@
-// const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
-
-// const url = 'mongodb://localhost:27017/conFusion';
-
-// MongoClient.connect(url, (err, db) => {
-
-// 	assert.equal(err, null);
-
-// 	console.log('connected successfully to server');
-
-// 	const collection = db.conllection("dishes");
-// 	collection.insertOne({"name":"Uthappizza", "description": "pizzaDesc"}, (err, result) => {
-// 		assert.equal(err, null);
-
-// 		console.log("After Insert: \n");
-// 		console.log(result.ops);
-
-// 		collection.find({}).toArray((err, docs) => {
-// 			assert.equal(err, null);
-
-// 			console.log("found: \n");
-// 			console.log(docs);
-
-// 			db.dropCollection("dishes", (err, result) => {
-// 				assert.equal(err, null);
-
-// 				db.close();
-// 			});
-// 		});
-// 	});
-
-// });
-
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
