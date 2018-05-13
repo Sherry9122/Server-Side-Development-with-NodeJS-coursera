b035f43e8dc4c4d87ed265ed87e628d21dc9cfec
Xueying Zheng <zhengxueyingbupt@gmail.com>
2018-05-13 16:14:54 -0400

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -12,27 +12,78 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
+    })
+    .then((dish) => {
+        console.log(dish);
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
 
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return db.collection('dishes').drop();
-        })
-        .then(() => {
-            return db.close();
-        })
-        .catch((err) => {
-            console.log(err);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
         });
 
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return db.collection('dishes').drop();
+    })
+    .then(() => {
+        return db.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+    // Dishes.create({
+    //     name: 'Uthapizza',
+    //     description: 'Test'
+    // })
+    // .then((dish) => {
+    //     console.log(dish);
+        
+    //     return Dishes.findByIdAndUpdate(dish._id, {
+    //             $set: {
+    //                 description: 'Updated Test'
+    //             }
+    //         }, {
+    //             new: true
+    //         })
+    //         .exec();
+    // })
+    // .then((dish) => {
+    //     console.log(dish);
+
+    //     dish.comments.push({
+    //         rating: 5,
+    //         comment: 'comment push',
+    //         author: 'XYZ'
+    //     });
+    //     return dish.save();
+    // })
+    // .then((dish) => {
+    //     console.log(dish);
+
+    //     return db.collection('dishes').drop();
+    // })
+    // .then(() => {
+    //     return db.close();        
+    // })
+    // .catch((err) => {
+
+    // });
 });
